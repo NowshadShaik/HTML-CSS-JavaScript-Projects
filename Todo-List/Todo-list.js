7d45f3e217f8ad94fb8366dfc8c8f910e8014d5e
Nowshad Shaik <nowshad7037@gmail.com>
2025-07-23 23:54:35 +0530

    Add grid in body for todo and css for it

--- Todo-List/Todo-list.js
+++ Todo-List/Todo-list.js
@@ -17,26 +17,47 @@ const todoList = [{
   id: '4',
   name: 'Task-4',
   date: '26/07/2025'
+},
+{
+  id: '5',
+  name: 'Task-5',
+  date: '25/07/2025'
+},
+{
+  id: '6',
+  name: 'Task-6',
+  date: '26/07/2025'
+},
+{
+  id: '7',
+  name: 'Task-7',
+  date: '27/07/2025'
 }]
 
 
 
 
 function renderTodo() {
-  let gridHtml = '';
+  let gridHtml = `
+  <div class="grid-header">Task Id</div>
+  <div class="grid-header">Task name</div>
+  <div class="grid-header">Task due by</div>
+  <div class="grid-header">status</div>
+  `;
 
   todoList.forEach((value, index) => {
     let element = `
-    <div>
-      <div>${value.id}</div>
-      <div>${value.name}</div>
-      <div>${value.date}</div>
-    </div>
+      <div class="grid-item">${value.id}</div>
+      <div class="grid-item">${value.name}</div>
+      <div class="grid-item">${value.date}</div>
+      <div class="grid-item">
+        <button class="status-button">Mark as complete</button>
+      </div>
     `;
     gridHtml += element;
   })
 
-  document.querySelector('.todo-grid').innerHTML = gridHtml;
+  document.querySelector('.todo-tasks-grid').innerHTML = gridHtml;
 }
 
 renderTodo();
